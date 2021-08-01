f7822f8bdc4493cccb63d2fb48a1db0630eb0f43
hector martinez <mejia.martinez2012@hotmail.es>
2021-07-31 22:18:04 -0600

    Guia 1 modificada

---
+++ GUIA 1/MvcPelicula/MvcPelicula/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPelicula.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //get:/HelloWorld/ 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET:/HelloWorld/Welcolme
+        public ActionResult Welcome(string nombre, int numVeces = 1)
+        {
+            ViewBag.Mensaje = "Hola" + nombre;
+            ViewBag.NumVeces = numVeces;
+            return View();
+        }
+
+        }
+}
---
+++ GUIA 1/MvcPelicula/MvcPelicula/Models/Pelicula.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+namespace MvcPelicula.Models
+{
+    public class Pelicula
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+        public string Genero { get; set; }
+        public decimal Precio { get; set; }
+    }
+    public class PeliculaDBContext : DbContext
+    {
+        public DbSet<Pelicula> Peliculas { get; set; }
+    }
+}
